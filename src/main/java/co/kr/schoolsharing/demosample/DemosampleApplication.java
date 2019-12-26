7a1033e3d8c12cf5df43c045ddda186cad9a514a
TaeYeongHWANG <taeyeong.hwang@schoolsharing.co.kr>
2019-12-27 08:21:59 +0900

    push sample project

---
+++ src/main/java/co/kr/schoolsharing/demosample/DemosampleApplication.java
@@ -0,0 +1,13 @@
+package co.kr.schoolsharing.demosample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemosampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemosampleApplication.class, args);
+    }
+
+}
